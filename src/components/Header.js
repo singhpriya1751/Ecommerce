ac7527bf44de1eb460b7022ec2e6dae10ecda50f
Priya <singhpriya17514@gmail.com>
2023-05-01 21:23:31 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="./images/banner.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Header;
